2d7b4bf1eb81f4d77c01e08b609873e23f412b3d
amarosuarez <amaro.suarez@iesnervion.es>
2025-01-20 14:06:05 +0100

    Agregar archivos de proyecto.

---
+++ ChatHub/ChatHub/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using ChatHub.Hubs;
+using ChatHub.Client.Pages;
+using ChatHub.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+// Agregado SignalR
+builder.Services.AddSignalR();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        ["application/octet-stream"]);
+});
+
+var app = builder.Build();
+
+// Agregado SignalR
+app.UseResponseCompression();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(ChatHub.Client._Imports).Assembly);
+
+// Agregado SignalR
+app.MapHub<ChatApp>("/chathub");
+
+app.Run();
+
+
