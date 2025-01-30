3e047136339b7964b218912f173ad020e709161b
amsuv <amaro.suarez@iesnervion.es>
2025-01-30 22:31:15 +0100

    ViewModel hecho

--- ChatMaui/MainPage.xaml.cs
+++ ChatMaui/MainPage.xaml.cs
@@ -12,108 +12,108 @@ namespace ChatMaui
         {
             InitializeComponent();
 
-            _connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5279/chathub")
-                .Build();
+            //_connection = new HubConnectionBuilder()
+            //    .WithUrl("http://localhost:5279/chathub")
+            //    .Build();
 
-            //_connection.On<MensajeUsuario>("ReceiveMessage", (mensajeUsuario) =>
+            ////_connection.On<MensajeUsuario>("ReceiveMessage", (mensajeUsuario) =>
+            ////{
+            ////    MainThread.BeginInvokeOnMainThread(() =>
+            ////    {
+            ////        chatMessages.Text += $"{mensajeUsuario.Usuario}: {mensajeUsuario.Mensaje} \n";
+            ////    });
+            ////});
+
+            //_connection.On<MensajeUsuario>("ReceiveMessage", ReceiveMessage);
+
+            //Task.Run(() => 
             //{
-            //    MainThread.BeginInvokeOnMainThread(() =>
-            //    {
-            //        chatMessages.Text += $"{mensajeUsuario.Usuario}: {mensajeUsuario.Mensaje} \n";
-            //    });
+            //    Dispatcher.Dispatch(async () => await _connection.StartAsync());
             //});
-
-            _connection.On<MensajeUsuario>("ReceiveMessage", ReceiveMessage);
-
-            Task.Run(() => 
-            {
-                Dispatcher.Dispatch(async () => await _connection.StartAsync());
-            });
         }
 
-        private async void ReceiveMessage(MensajeUsuario mensajeUsuario)
-        {
-            Dispatcher.Dispatch(async () => chatMessages.Text += $"{mensajeUsuario.Usuario}: {mensajeUsuario.Mensaje} \n");
-        }
+        //private async void ReceiveMessage(MensajeUsuario mensajeUsuario)
+        //{
+        //    Dispatcher.Dispatch(async () => chatMessages.Text += $"{mensajeUsuario.Usuario}: {mensajeUsuario.Mensaje} \n");
+        //}
 
-        private async void OnCounterClicked(object sender, EventArgs e)
-        {   
-            if (!string.IsNullOrEmpty(_room))
-            {
-                MensajeUsuario mensajeUsuario = new MensajeUsuario();
-                mensajeUsuario.Sala = groupName.Text;
-                mensajeUsuario.Usuario = myUsername.Text;
-                mensajeUsuario.Mensaje = myChatMessages.Text;
+        //private async void OnCounterClicked(object sender, EventArgs e)
+        //{   
+        //    if (!string.IsNullOrEmpty(_room))
+        //    {
+        //        MensajeUsuario mensajeUsuario = new MensajeUsuario();
+        //        mensajeUsuario.Sala = groupName.Text;
+        //        mensajeUsuario.Usuario = myUsername.Text;
+        //        mensajeUsuario.Mensaje = myChatMessages.Text;
 
-                await _connection.InvokeCoreAsync("SendMessage", args: new[]
-                    { mensajeUsuario});
+        //        await _connection.InvokeCoreAsync("SendMessage", args: new[]
+        //            { mensajeUsuario});
 
-                myChatMessages.Text = String.Empty;
-            }
-            else
-            {
-                error.Text = "Debes unirte a un grupo";
-                error.IsVisible = true;
-            }
-        }
+        //        myChatMessages.Text = String.Empty;
+        //    }
+        //    else
+        //    {
+        //        error.Text = "Debes unirte a un grupo";
+        //        error.IsVisible = true;
+        //    }
+        //}
 
-        private async void joinGroupButton_Clicked(object sender, EventArgs e)
-        {
-            String group = groupName.Text;
+        //private async void joinGroupButton_Clicked(object sender, EventArgs e)
+        //{
+        //    String group = groupName.Text;
 
-            // Comprueba que haya escrito un grupo
-            if (!string.IsNullOrEmpty(group))
-            {
-                // Comprueba si ya estaba en un grupo y que no sea el mismo
-                if (!string.IsNullOrEmpty(_room) && !_room.Equals(group))
-                {
-                    // Sale del grupo anterior
-                    await _connection.InvokeCoreAsync("LeaveRoom", args: new[]
-                        { _room });
+        //    // Comprueba que haya escrito un grupo
+        //    if (!string.IsNullOrEmpty(group))
+        //    {
+        //        // Comprueba si ya estaba en un grupo y que no sea el mismo
+        //        if (!string.IsNullOrEmpty(_room) && !_room.Equals(group))
+        //        {
+        //            // Sale del grupo anterior
+        //            await _connection.InvokeCoreAsync("LeaveRoom", args: new[]
+        //                { _room });
 
-                    // Unimos al nuevo grupo
-                    addGroup(group);
-                } else
-                {
-                    // Unimos al nuevo grupo
-                    addGroup(group);
-                }
-            } else
-            {
-                error.Text = "Escribe el nombre de un grupo";
-                error.IsVisible = true;
-            }
+        //            // Unimos al nuevo grupo
+        //            addGroup(group);
+        //        } else
+        //        {
+        //            // Unimos al nuevo grupo
+        //            addGroup(group);
+        //        }
+        //    } else
+        //    {
+        //        error.Text = "Escribe el nombre de un grupo";
+        //        error.IsVisible = true;
+        //    }
 
-        }
+        //}
 
-        private async void addGroup(String group)
-        {
-            // Limpiamos
-            _room = "";
+        //private async void addGroup(String group)
+        //{
+        //    // Limpiamos
+        //    _room = "";
 
-            // Se une al grupo nuevo
-            await _connection.InvokeCoreAsync("JoinRoom", args: new[]
-                { group });
+        //    // Se une al grupo nuevo
+        //    await _connection.InvokeCoreAsync("JoinRoom", args: new[]
+        //        { group });
 
-            _room = group;
-            error.IsVisible = false;
-            chatMessages.Text = "";
-        }
+        //    _room = group;
+        //    error.IsVisible = false;
+        //    chatMessages.Text = "";
+        //}
 
-        private async void removeGroupButton_Clicked(object sender, EventArgs e)
-        {
-            String group = groupName.Text;
+        //private async void removeGroupButton_Clicked(object sender, EventArgs e)
+        //{
+        //    String group = groupName.Text;
 
-            await _connection.InvokeCoreAsync("LeaveRoom", args: new[]
-                { _room });
+        //    await _connection.InvokeCoreAsync("LeaveRoom", args: new[]
+        //        { _room });
 
-            _room = "";
-            error.IsVisible = true;
-            error.Text = "Has salido del grupo " + group;
-            groupName.Text = "";
-            chatMessages.Text = "";
-        }
+        //    error.IsVisible = true;
+        //    error.Text = "Has salido del grupo " + _room;
+        //    _room = "";
+        //    groupName.Text = "";
+        //    chatMessages.Text = "";
+        //}
     }
 
 }
--- ChatMaui/VM/ChatVM.cs
+++ ChatMaui/VM/ChatVM.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Models;
 using ViewModels.Utilidades;
 
 namespace ChatMaui.VM
 {
-    public class ChatVM
+    public class ChatVM : INotifyPropertyChanged
     {
         #region Atributos
-        private String room;
+        private readonly HubConnection _connection;
+        private String _room;
         private String group;
         private String name;
         private String message;
+        private String chatMessages;
         private DelegateCommand joinGroupCommand;
         private DelegateCommand exitGroupCommand;
         private DelegateCommand sendMessageCommand;
@@ -22,20 +28,32 @@ namespace ChatMaui.VM
         #region Propiedades
         public String Group
         {
-            get { return room; }
-            set { room = value; }
+            get { return group; }
+            set { group = value; }
         }
 
         public String Name
         {
             get { return name; }
-            set { name = value; } 
+            set { 
+                name = value;
+                sendMessageCommand.RaiseCanExecuteChanged();
+            } 
         }
 
         public String Message
         {
             get { return message; }
-            set { message = value; }
+            set { 
+                message = value;
+                sendMessageCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public String ChatMessages
+        {
+            get { return chatMessages; }
+            set { chatMessages = value; }
         }
 
         public DelegateCommand JoinGroupCommand
@@ -57,6 +75,144 @@ namespace ChatMaui.VM
         #region Constructores
         public ChatVM()
         {
+            // Commands
+            joinGroupCommand = new DelegateCommand(joinGroupCommandExecuted, joinGroupCommandCanExecute);
+            exitGroupCommand = new DelegateCommand(exitGroupCommandExecuted, exitGroupCommandCanExecute);
+            sendMessageCommand = new DelegateCommand(sendMessageCommandExecuted, sendMessageCommandCanExecute);
+
+            // Creamos la conexión
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5279/chathub")
+                .Build();
+
+            // Nos conectamos
+            Task.Run(async () =>
+            {
+                await MainThread.InvokeOnMainThreadAsync(async () => await _connection.StartAsync());
+            });
+
+            // Recibo los mensajes de forma automática
+            _connection.On<MensajeUsuario>("ReceiveMessage", receiveMessage);
+        }
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Método que recibe los mensajes automáticamente y los pinta en la pantalla<br>
+        /// Pre: Ninguno</br>
+        /// Post: Ninguno
+        /// </summary>
+        /// <param name="mensajeUsuario">Nuevo mensaje a pintar</param>
+        private async void receiveMessage(MensajeUsuario mensajeUsuario)
+        {
+            await MainThread.InvokeOnMainThreadAsync(async () => chatMessages  += $"{mensajeUsuario.Usuario}: {mensajeUsuario.Mensaje}\n");
+            NotifyPropertyChanged(nameof(ChatMessages));
+        }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Función que une al usuario al grupo especificado <br>
+        /// Pre: Debe haberse especificado el grupo</br>
+        /// Post: Ninguno
+        /// </summary>
+        public async void joinGroupCommandExecuted()
+        {
+            if (!string.IsNullOrEmpty(group))
+            {
+                await _connection.InvokeCoreAsync("JoinRoom", args: new[]
+                 { group });
+
+                _room = group;
+
+                sendMessageCommand.RaiseCanExecuteChanged();
+                joinGroupCommand.RaiseCanExecuteChanged();
+                exitGroupCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        /// <summary>
+        /// Función que determina si el command para unir al usuario al grupo está activo o no<br>
+        /// Pre: Ninguno</br>
+        /// Post: Ninguno
+        /// </summary>
+        /// <returns>Puede ejecutarse o no el command</returns>
+        public bool joinGroupCommandCanExecute()
+        {
+            return string.IsNullOrEmpty(_room);
+        }
+
+        /// <summary>
+        /// Función que saca al usuario del grupo en el que se encuentra<br>
+        /// Pre: Ninguno</br>
+        /// Post: Ninguno
+        /// </summary>
+        public async void exitGroupCommandExecuted()
+        {
+            await _connection.InvokeCoreAsync("LeaveRoom", args: new[]
+                { _room });
+
+            chatMessages = "";
+            group = "";
+            _room = group;
+            NotifyPropertyChanged(nameof(ChatMessages));
+            NotifyPropertyChanged(nameof(Group));
+
+            joinGroupCommand.RaiseCanExecuteChanged();
+            exitGroupCommand.RaiseCanExecuteChanged();
+        }
+
+        /// <summary>
+        /// Función que determina si el command para sacar al usuario del grupo está activo o no<br>
+        /// Pre: Ninguno</br>
+        /// Post: Ninguno
+        /// </summary>
+        /// <returns>Puede ejecutarse o no el command</returns>
+        public bool exitGroupCommandCanExecute()
+        {
+           return !string.IsNullOrEmpty(_room);
+        }
+        
+        /// <summary>
+        /// Función que envía el mensaje<br>
+        /// Pre: Debe haberse escrito el nombre y el mensaje</br>
+        /// Post: Ninguno
+        /// </summary>
+        public async void sendMessageCommandExecuted()
+        {
+            MensajeUsuario mensajeUsuario = new MensajeUsuario();
+            mensajeUsuario.Sala = group;
+            mensajeUsuario.Usuario = name;
+            mensajeUsuario.Mensaje = message;
+
+            await _connection.InvokeCoreAsync("SendMessage", args: new[]
+                { mensajeUsuario});
+
+            message = String.Empty;
+            NotifyPropertyChanged(nameof(Message));
+        }
+
+        /// <summary>
+        /// Función que determina si el command para enviar el mensaje está activo o no <br>
+        /// Pre: Ninguno</br>
+        /// Post: Ninguno
+        /// </summary>
+        /// <returns>Puede ejecutarse o noel command</returns>
+        public bool sendMessageCommandCanExecute()
+        {
+            return !string.IsNullOrEmpty(_room) && !string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(message);
+        }
+        #endregion
+
+        #region Notify
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+
+        {
+
+            PropertyChanged?.Invoke(this, new
+            PropertyChangedEventArgs(propertyName));
 
         }
         #endregion
